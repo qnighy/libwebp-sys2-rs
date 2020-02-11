e28c7a20ba1afd2df1129d432cefda001d8975cb
Masaki Hara <ackie.h.gmai@gmail.com>
2020-02-11 19:04:09 +0900

    Fix types.rs

--- libwebp-sys/src/types.rs
+++ libwebp-sys/src/types.rs
@@ -1,3 +1,6 @@
+#[cfg(feature = "0.5")]
+use std::os::raw::*;
+
 #[allow(non_snake_case)]
 pub const fn WEBP_ABI_IS_INCOMPATIBLE(a: u16, b: u16) -> bool {
     (a >> 8) != (b >> 8)
@@ -6,7 +9,7 @@ pub const fn WEBP_ABI_IS_INCOMPATIBLE(a: u16, b: u16) -> bool {
 #[cfg_attr(not(test), link(name = "webp"))]
 extern "C" {
     #[cfg(feature = "1.1")]
-    pub fn WebPMalloc(size: usize);
+    pub fn WebPMalloc(size: usize) -> *mut c_void;
     #[cfg(feature = "0.5")]
     pub fn WebPFree(ptr: *mut c_void);
 }
