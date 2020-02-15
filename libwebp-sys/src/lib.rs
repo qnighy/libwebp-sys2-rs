ff900d953e8a18a211d13d7e067a99b2e73c254a
Masaki Hara <ackie.h.gmai@gmail.com>
2020-02-15 23:05:07 +0900

    Add demux and mux features

--- libwebp-sys/src/lib.rs
+++ libwebp-sys/src/lib.rs
@@ -2,17 +2,23 @@
 extern crate cfg_if;
 
 pub use crate::decode::*;
+#[cfg(feature = "demux")]
 pub use crate::demux::*;
 pub use crate::encode::*;
 pub use crate::format_constants::*;
+#[cfg(feature = "mux")]
 pub use crate::mux::*;
+#[cfg(any(feature = "mux", feature = "demux"))]
 pub use crate::mux_types::*;
 pub use crate::types::*;
 
 mod decode;
+#[cfg(feature = "demux")]
 mod demux;
 mod encode;
 mod format_constants;
+#[cfg(feature = "mux")]
 mod mux;
+#[cfg(any(feature = "mux", feature = "demux"))]
 mod mux_types;
 mod types;
